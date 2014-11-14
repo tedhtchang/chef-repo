ad35ed5217dbb9ee35a571fb70687165c8f7cc7c
Ted Chang <tedhtchang@gmail.com>
2014-11-13 21:46:05 -0800

    Removed yum version from metadata.rb and updated attribute default.rb

--- cookbooks/contrail/metadata.rb
+++ cookbooks/contrail/metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures contrail'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'yum', '3.4.1'
+depends 'yum'
 depends 'python'
